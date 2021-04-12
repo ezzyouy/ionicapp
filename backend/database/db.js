c09713c3c426dd80abcb6a12b41370de38f0d5a7
ezzyouy <brahim.ezzyouy@gmail.com>
2021-04-12 13:21:02 +0000

    first commit

---
+++ backend/database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  db: 'mongodb+srv://Smile:vEkvy3iAWa1YYMDT@cluster0.5w1dl.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'add-song',
+    loadChildren: () => import('./add-song/add-song.module').then( m => m.AddSongPageModule)
+  },
+  {
+    path: 'edit-song/:id',
+    loadChildren: () => import('./edit-song/edit-song.module').then( m => m.EditSongPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
